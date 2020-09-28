1ee41f4014af87a75e080dea2c5393eb74d7f964
resession <plainstud@gmail.com>
2020-09-28 06:49:01 -0500

    add files

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: tru
     }
 });
 
+app.set('trust proxy')
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
