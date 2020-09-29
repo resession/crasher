a826dd7214a2d19044fca8aee782c78a3bd18863
resession <plainstud@gmail.com>
2020-09-28 21:56:49 -0500

    add files

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const rateLimit = require("express-rate-limit")
 const MongoStore = require('rate-limit-mongo')
 const morgan = require('morgan')
 const cors = require('cors')
-const http = require('http')
-const https = require('https')
-const fs = require('fs')
+// const http = require('http')
+// const https = require('https')
+// const fs = require('fs')
 
 const app = express()
 mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true}, error => {
@@ -40,14 +40,14 @@ app.get('*', (req, res) => {
     return res.status(400).json('error')
 })
 
-// app.listen(process.env.PORT, process.env.HOST, () => {
-//     console.log('listening')
+app.listen(process.env.PORT, () => {
+    console.log('listening')
+})
+
+// http.createServer(app).listen(process.env.HTTPPORT, process.env.HOST, () => {
+//     console.log('http listening')
 // })
 
-http.createServer(app).listen(process.env.HTTPPORT, process.env.HOST, () => {
-    console.log('http listening')
-})
-
-https.createServer({cert: fs.readFileSync('/etc/letsencrypt/live/krasher.website/cert.pem', 'utf8'), key: fs.readFileSync('/etc/letsencrypt/live/krasher.website/privkey.pem', 'utf8'), ca: fs.readFileSync('/etc/letsencrypt/live/krasher.website/chain.pem', 'utf8')}, app).listen(process.env.HTTPSPORT, process.env.HOST, () => {
-    console.log('https listening')
-})
+// https.createServer({cert: fs.readFileSync('/etc/letsencrypt/live/krasher.website/cert.pem', 'utf8'), key: fs.readFileSync('/etc/letsencrypt/live/krasher.website/privkey.pem', 'utf8'), ca: fs.readFileSync('/etc/letsencrypt/live/krasher.website/chain.pem', 'utf8')}, app).listen(process.env.HTTPSPORT, process.env.HOST, () => {
+//     console.log('https listening')
+// })
