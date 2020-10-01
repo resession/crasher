c83b11eeca76e67d388258a47dc5b1fe47be570d
resession <plainstud@gmail.com>
2020-10-01 14:15:37 -0500

    add files

--- folder/data.js
+++ folder/data.js
@@ -4,7 +4,7 @@ const secretKey = process.env.SECRETKEY
 // const urlRecaptcha = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRETKEY}&response=`
 
 function verifyRecaptcha(req, res, next){
-    if(!req.body.token || req.body.token !== 'string'){
+    if(!req.body.token || typeof(req.body.token) !== 'string'){
         return res.status(400).json('error')
     } else {
         axios.post('https://www.google.com/recaptcha/api/siteverify', {}, {params: {secret: secretKey, response: req.body.token}}).then(data => {
