9193f7f9f75ab041bf944edd0f146ee28871f7a9
resession <plainstud@gmail.com>
2020-10-01 15:07:11 -0500

    add files

--- folder/data.js
+++ folder/data.js
@@ -8,7 +8,7 @@ function verifyRecaptcha(req, res, next){
         return res.status(400).json('error')
     } else {
         axios.post('https://www.google.com/recaptcha/api/siteverify', {}, {params: {secret: secretKey, response: req.body.token}}).then(data => {
-            if(!data.success || !data.score){
+            if(!data.success || data.score < 0.1){
                 return res.status(400).json('error')
             } else {
                 next()
